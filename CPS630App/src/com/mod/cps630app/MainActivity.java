8f2f4470e459e89bf42a45b263f7b5d946fa79a5
Tyler Sutherland <tylerksutherland+github@gmail.com>
2014-03-04 19:03:09 -0500

    Added options menu to main

--- CPS630App/src/com/mod/cps630app/MainActivity.java
+++ CPS630App/src/com/mod/cps630app/MainActivity.java
@@ -3,6 +3,7 @@ package com.mod.cps630app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -18,6 +19,13 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_display, menu);
+		return true;
 	}
 
 	public void showStaticLocations(View view) {
